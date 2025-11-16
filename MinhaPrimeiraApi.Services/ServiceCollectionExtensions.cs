25b435bd5b78583b20d348f033bdd4cee1df13e3
Jhones Michael <82251374+jhoneshark@users.noreply.github.com>
2025-11-16 00:57:13 -0300

    added clean architecture

--- MinhaPrimeiraApi.Services/ServiceCollectionExtensions.cs
+++ MinhaPrimeiraApi.Services/ServiceCollectionExtensions.cs
@@ -12,5 +12,9 @@ public static class ServiceCollectionExtensions
 
     public static void AddApplicationServices(IServiceCollection services)
     {
+        // Está registrando a injeção de dependência, dizendo ao ASP.NET Core para usar CategoriesRepository sempre que alguém pedir por ICategoryRepository
+        // services.AddScoped<ICategoryRepository, CategoriesRepository>();
+        // services.AddScoped<IProductsRepository, ProductsRepository>();
+        // services.AddScoped<IUnitOfWork, UnitOfWork>();
     }
 }
